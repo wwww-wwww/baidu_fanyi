a0074e6ab103219a953fa7a65167e43e470ebab5
w <wvvwvvvvwvvw@gmail.com>
2020-09-04 20:07:36 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="baidu_fanyi",
+  version="0.0.2",
+  author="wwwwwwww",
+  author_email="wvvwvvvvwvvw@gmail.com",
+  description="Baidu translation API",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/wwww-wwww/baidu_fanyi",
+  packages=setuptools.find_packages(),
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  python_requires='>=3.6',
+)
